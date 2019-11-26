6347b388fb89bdd23fd5abd5f43f46b1e13b2c5a
mishazawa <macintoshkrug@gmail.com>
2019-11-26 22:38:36 +0200

    clarify formula

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use minifb::{Window, Key, WindowOptions, MouseMode};
 
 const WIDTH: usize = 600;
 const HEIGHT: usize = 600;
-const ITERATIONS: f64 = 100.0;
+const FRACTAL_DEPTH: u32 = 512;
 
 
 fn main() {
@@ -24,65 +24,64 @@ fn main() {
       }
     };
 
-  let mut range = 4.0;
+  let mut range = 2.0;
 
   while window.is_open() && !window.is_key_down(Key::Escape) {
-      let mut x_mouse = 0.0;
-      let mut y_mouse = 0.0;
-      window.get_mouse_pos(MouseMode::Discard).map(|(x, y)| {
-        x_mouse = map(x as f64, 0., WIDTH as f64, -1. , 1.);
-        y_mouse = map(y as f64, 0., HEIGHT as f64, -1. , 1.);
-      });
-      let x_min = -2.;
-      let y_min = -2.;
+    let mut x_mouse = 0.0;
+    let mut y_mouse = 0.0;
 
-      let x_max = x_min + range;
-      let y_max = y_min + range;
-      let dx = (x_max - x_min) / WIDTH as f64;
-      let dy = (y_max - y_min) / HEIGHT as f64;
-      let mut y_step = y_min;
+    window.get_mouse_pos(MouseMode::Discard).map(|(x, y)| {
+      x_mouse = map(x as f64, 0., WIDTH as f64, -1. , 1.);
+      y_mouse = map(y as f64, 0., HEIGHT as f64, -1. , 1.);
+    });
 
-      for y in 0..HEIGHT {
+    let x_min = 0. - range;
+    let y_min = 0. - range;
 
-        let mut x_step = x_min;
+    let x_max = 0. + range;
+    let y_max = 0. + range;
 
-        for x in 0..WIDTH {
-          let mut a = x_step as f64;
-          let mut b = y_step as f64;
+    for y in 0..HEIGHT {
+      for x in 0..WIDTH {
+        let mut real = map(x as f64, 0., WIDTH as f64, x_min , x_max);
+        let mut imag = map(y as f64, 0., HEIGHT as f64, y_min, y_max);
 
-          let mut n = 0.;
+        let origin_re = real;
+        let origin_im = imag;
 
-          while n < ITERATIONS {
-            let aa = a * a;
-            let bb = b * b;
-            let twoab = 2.0 * a * b;
-            a = aa - bb + x_mouse as f64;
-            b = twoab + y_mouse as f64;
-            if aa + bb > 16.0 {
-              break;  // Bail
-            }
-            n += 1.0;
+        let mut n = 0;
+
+        while n < FRACTAL_DEPTH {
+          let re = real.powf(2.) - imag.powf(2.);
+          let im = 2. * real * imag;
+
+          real = re + x_mouse;
+          imag = im + y_mouse;
+
+          if (real + imag).abs() > 16.0 {
+            break;  // Bail
           }
-
-          let screen_pos = ((y as usize) * WIDTH) + x as usize;
-          if ITERATIONS == n {
-            buffer[screen_pos] = 0x00;
-          } else {
-            buffer[screen_pos] = n as u32 * 16 % 255;
-          }
-
-          x_step += dx;
+          n += 1;
         }
-        y_step += dy;
+
+        let screen_pos = ((y as usize) * WIDTH) + x as usize;
+        if FRACTAL_DEPTH == n {
+          buffer[screen_pos] = 0x00;
+        } else {
+          buffer[screen_pos] = n * 16 % 255;
+        }
       }
+    }
 
-      window.get_scroll_wheel().map(|scroll| {
-        range += 0.1 * scroll.1 as f64;
-        println!("Scrolling {}, {}:{}", range, dx, dy);
-      });
+    window.get_scroll_wheel().map(|scroll| {
+      if scroll.1 != 0. {
+        range /= 2.;
+      }
+      println!("Zoom {}", range);
+    });
 
-      // We unwrap here as we want this code to exit if it fails
-      window.update_with_buffer(&buffer).unwrap();
+    // We unwrap here as we want this code to exit if it fails
+    window.update_with_buffer(&buffer).unwrap();
   }
 }
 
